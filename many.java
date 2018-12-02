e109ab97126c87c08eaebbda03e1f51112ae861a
wyk <781250345@qq.com>
2018-12-02 21:48:37 +0800

    'mod'

--- many.java
+++ many.java
@@ -2206,7 +2206,7 @@ public class many {
         int tmp ;
         int chengji = 1;
         for (int i = 2; i <= n;i+=2){
-            if (i%10==0) {
+            if (i%10==0) {20
                 count++;
                 continue;
             }
@@ -2221,6 +2221,30 @@ public class many {
         return count;
     }
     */
+
+//278
+  /*  public int firstBadVersion(int n) {
+        return firstBadVersionhelp(1,n);
+    }
+
+    public int firstBadVersionhelp(int left,int right){
+        int mid = left ;
+        while (left <= right){
+            mid = (left/2+right/2);
+            if (isBadVersion(mid) && !isBadVersion(mid-1)){
+                return mid;
+            }else if (isBadVersion(mid) && isBadVersion(mid-1)){
+                right = mid-1;
+            }else if (!isBadVersion(mid) && isBadVersion(mid+1) ){
+                return mid+1;
+            }else {
+
+                left = mid + 1;
+            }
+        }
+        return mid;
+    }*/
+
 
     public int trailingZeroes(int n) {
         int sum = 0;
