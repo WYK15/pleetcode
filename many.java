354ba13a4db2c931cfd9a436efe067791681aabe
wangyankun <wangyankun@iyunxiao.com>
2018-12-05 17:19:17 +0800

    add

--- many.java
+++ many.java
@@ -2348,6 +2348,25 @@ public class many {
         return res;
     }
 
+    //712
+    public int minimumDeleteSum(String s1, String s2) {
+        int len1 = s1.length(),len2 = s2.length();
+        int[][] dp = new int[len1][len2];
+        for (int i = 1;i < len1-1;i++){
+            dp[i][0] = dp[i-1][0] + s1.charAt(i);
+        }
+        for (int j = 1;j < len2-1;j++){
+            dp[0][j] = dp[0][j-1] + s2.charAt(j);
+        }
+        for (int i = 0;i < len1;i++){
+            for (int j = 0;j < len2;j++){
+                int tmp = s1.charAt(i) == s2.charAt(j) ? 0 : s1.charAt(i)+s2.charAt(j);
+                dp[i][j] = Math.min(dp[i-1][j]+s1.charAt(i),Math.min(dp[i][j-1]+s2.charAt(j),dp[i-1][j-1]+tmp));
+            }
+        }
+        return dp[len1-1][len2-1];
+    }
+
     public static void main(String[] args) throws CloneNotSupportedException {
         many m = new many();
       /* stu stu1 = new stu();
