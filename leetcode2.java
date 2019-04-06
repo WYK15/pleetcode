2b13efffa702a8c389f44fea1ec69e1e7b68e028
leowang <781250345@qq.com>
2019-04-06 09:39:34 +0800

    add:496

--- leetcode2.java
+++ leetcode2.java
@@ -689,6 +689,24 @@ public class leetcode2 {
         return result;
     }
 
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        if (nums2.length == 0) return new int[]{};
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums2[0]);
+        for (int i = 1; i < nums2.length;i++){
+            while (!stack.isEmpty()&&nums2[i] > stack.peek()){
+                hm.put(stack.pop(),nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length;i++){
+            result[i] = hm.getOrDefault(nums1[i],-1);
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
         leetcode2 m = new leetcode2();
