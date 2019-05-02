4666f3dbb7c62026ec0429f7d9cf1a3ce17564c9
leowang <781250345@qq.com>
2019-05-02 23:29:56 +0800

    add:235

--- leetcode2.java
+++ leetcode2.java
@@ -1202,6 +1202,15 @@ public class leetcode2 {
         return Math.max(result,count);
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val < q.val) return root;
+        if (root.val < p.val && root.val > q.val) return root;
+        if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right,p,q);
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left,p,q);
+        return root;
+    }
+
+
 
 }
 
