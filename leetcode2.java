cfc7c5524e6e8f4382017482649fc62790b15167
leowang <781250345@qq.com>
2019-05-22 10:33:59 +0800

    add:561

--- leetcode2.java
+++ leetcode2.java
@@ -1435,6 +1435,15 @@ public class leetcode2 {
         return A;
     }
 
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length,sum = 0;
+        for (int i = 0;i < len;i+=2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
 
     public static void main(String[] args) {
 
