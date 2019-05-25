5a24d1864c1520fdb07153d4ec5762d49a826e06
leowang <781250345@qq.com>
2019-05-25 23:13:11 +0800

    add:86

--- leetcode2.java
+++ leetcode2.java
@@ -1620,9 +1620,31 @@ public class leetcode2 {
         return result;
     }
 
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyHead1 = new ListNode(0);
+        ListNode dummyHead2 = new ListNode(0);
+        ListNode node1 = dummyHead1;
+        ListNode node2 = dummyHead2;
+        while (head != null) {
+            if (head.val < x) {
+                node1.next = head;
+                head = head.next;
+                node1 = node1.next;
+                node1.next = null;
+            } else {
+                node2.next = head;
+                head = head.next;
+                node2 = node2.next;
+                node2.next = null;
+            }
+        }
+        node1.next = dummyHead2.next;
+        return dummyHead1.next;
+    }
+
     public static void main(String[] args) {
        leetcode2 l = new leetcode2();
-        System.out.println(l.maxSumTwoNoOverlap(new int[]{2,1,5,6,0,9,5,0,3,8},4,3));
+
     }
 
 }
